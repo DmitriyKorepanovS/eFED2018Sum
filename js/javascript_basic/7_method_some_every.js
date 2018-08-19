a71f2f9af3060d0f7ff3be198eea3341579accd1
DmitriyKorepanov <dmitriykorepanovs@gmail.com>
2018-08-20 01:37:47 +0400

    update

--- js/javascript_basic/7_method_some_every.js
+++ js/javascript_basic/7_method_some_every.js
@@ -1,9 +1,9 @@
-function some(array) {
+function some(array, func) {
     var arr = array;
     var i, length = arr.length;
     for (i = 0; i < length; i = i + 1) {
 
-        if (Number.isNaN(arr[i])) {
+        if (func(arr[i])) {
             return true;
         }
     }
@@ -11,24 +11,23 @@ function some(array) {
 }
 
 
-function every(array) {
+function every(array, func) {
     var arr = array;
 
     var i, length = arr.length;
     for (i = 0; i < length; i = i + 1) {
 
-        if (!Number.isNaN(arr[i])) {
+        if (!func(arr[i])) {
             return false;
         }
         return true;
     }
 }
 
-console.log(every([1, 4, NaN, 6]));
+console.log(every([1, 4, NaN, 6], Number.isNaN));
 
-console.log(every([NaN, NaN]));
+console.log(every([NaN, NaN], Number.isNaN));
 
-console.log(some([1, 2, 6]));
+console.log(some([1, 2, 6], Number.isNaN));
 
-console.log(some([1, 4, NaN, 6]));
-
+console.log(some([1, 4, NaN, 6], Number.isNaN));
