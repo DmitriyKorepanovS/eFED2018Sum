72a434d437b9785efc01c803813d8dd238fa8b18
DmitriyKorepanov <dmitriykorepanovs@gmail.com>
2018-09-02 15:43:02 +0400

    update function Button

--- js/index.js
+++ js/index.js
@@ -1,93 +1,45 @@
-var fiveDaysContainer = document.getElementById('weathertable-5days');
-var elements = document.querySelectorAll('.time-of-day-temperature');
+var BtnTemperatureProbabilityWind = document.getElementById('button-temperature-probability-wind');
+var elementButtonrow = document.querySelectorAll('.buttonrow-button-noactive');
+console.log(elementButtonrow)
 
-fiveDaysContainer.addEventListener("click", switchInfo);
+BtnTemperatureProbabilityWind.addEventListener("click", switchButtonStatistic)
 
-function switchInfo(event) {
-  if (event.target.className === 'btnNext') {
-    showNext()
-  }
-  if (event.target.className === 'btnPrevious') {
-    showPrevious()
+function switchButtonStatistic() {
+
+  if (event.target.className === 'buttonrow-button-noactive') {
+    console.log(event.target);
+    showButton()
   }
 }
 
-function showNext() {
-  var indexNext;
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
+function showButton() {
+  for (var i = 0; i < elementButtonrow.length; i++) {
+    if (elementButtonrow[i].classList.contains('buttonrow-button-active')) {
+      elementButtonrow[i].classList.remove('buttonrow-button-active');
 
-      if (i === elements.length - 1) {
-        elements[0].classList.add('activex')
-        indexNext = 0;
-        break
-      }
-      indexNext = i + 1;
-      elements[indexNext].classList.add('activex')
+    }
+    event.target.classList.add('buttonrow-button-active');
+  }
+  for (var i = 0; i < elementButtonrow.length; i++) {
+    if (elementButtonrow[i].classList.contains('buttonrow-button-active')) {
+      var indexDiv = i;
+    }
+  }
+  showSection(indexDiv)
+}
+
+
+
+var Section= document.querySelectorAll('.probability-of-precipitation');
+
+function showSection(indexDiv) {
+  for (var i = 0; i < Section.length; i++) {
+    if (Section[i].classList.contains('activeSection')) {
+      Section[i].classList.remove('activeSection')
+      Section[indexDiv].classList.add('activeSection')
       break
     }
   }
-  showButton(indexNext)
-}
 
-function showPrevious() {
-  var indexPrevious;
-  for (var i = 0; i < elements.length; i++) {
 
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
-
-      if (i === 0) {
-        elements[elements.length - 1].classList.add('activex');
-        indexPrevious = elements.length - 1;
-        break
-      }
-      indexPrevious = i - 1;
-      elements[indexPrevious].classList.add('activex')
-      break
-    }
-  }
-  showButton(indexPrevious)
-}
-
-var buttonForDay = document.getElementById('buttonForDay');
-var elementButtonDayOfWeek = document.querySelectorAll('.buttonDayOfWeek');
-
-buttonForDay.addEventListener("click", switchButton);
-
-function switchButton(event) {
-  if (event.target.className === 'buttonDayOfWeek') {
-    for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-      if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
-        elementButtonDayOfWeek[i].classList.remove('activeDay')
-      }
-      event.target.classList.add('activeDay');
-    }
-    for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-      if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
-        var index = i;
-      }
-    }
-    showDay(index);
-  }
-}
-
-function showDay(index) {
-  for (var i = 0; i < elements.length; i++) {
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
-      elements[index].classList.add('activex')
-      break
-    }
-  }
-}
-
-function showButton(position) {
-  for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-    if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
-      elementButtonDayOfWeek[i].classList.remove('activeDay')
-    }
-  }
-  elementButtonDayOfWeek[position].classList.add('activeDay');
 }
