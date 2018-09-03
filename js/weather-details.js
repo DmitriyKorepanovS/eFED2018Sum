5bebe64302abbc07df7ef5da83f428b35176b5d7
DmitriyKorepanov <dmitriykorepanovs@gmail.com>
2018-09-03 12:21:22 +0400

    update JS add new variables

--- js/weather-details.js
+++ js/weather-details.js
@@ -1,6 +1,6 @@
 var fiveDaysContainer = document.getElementById('weathertable-5days');
 var elements = document.querySelectorAll('.time-of-day-temperature');
-
+var classForDay = 'activex';
 fiveDaysContainer.addEventListener("click", switchInfo);
 
 function switchInfo(event) {
@@ -15,16 +15,16 @@ function switchInfo(event) {
 function showNext() {
   var indexNext;
   for (var i = 0; i < elements.length; i++) {
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
+    if (elements[i].classList.contains(classForDay)) {
+      elements[i].classList.remove(classForDay)
 
       if (i === elements.length - 1) {
-        elements[0].classList.add('activex')
+        elements[0].classList.add(classForDay)
         indexNext = 0;
         break
       }
       indexNext = i + 1;
-      elements[indexNext].classList.add('activex')
+      elements[indexNext].classList.add(classForDay)
       break
     }
   }
@@ -35,16 +35,16 @@ function showPrevious() {
   var indexPrevious;
   for (var i = 0; i < elements.length; i++) {
 
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
+    if (elements[i].classList.contains(classForDay)) {
+      elements[i].classList.remove(classForDay)
 
       if (i === 0) {
-        elements[elements.length - 1].classList.add('activex');
+        elements[elements.length - 1].classList.add(classForDay);
         indexPrevious = elements.length - 1;
         break
       }
       indexPrevious = i - 1;
-      elements[indexPrevious].classList.add('activex')
+      elements[indexPrevious].classList.add(classForDay)
       break
     }
   }
@@ -53,6 +53,8 @@ function showPrevious() {
 
 var buttonForDay = document.getElementById('buttonForDay');
 var elementButtonDayOfWeek = document.querySelectorAll('.buttonDayOfWeek');
+var classForChange = 'activeDay';
+
 
 buttonForDay.addEventListener("click", switchButton);
 
@@ -60,13 +62,13 @@ function switchButton(event) {
   var index;
   if (event.target.className === 'buttonDayOfWeek') {
     for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-      if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
-        elementButtonDayOfWeek[i].classList.remove('activeDay')
+      if (elementButtonDayOfWeek[i].classList.contains(classForChange)) {
+        elementButtonDayOfWeek[i].classList.remove(classForChange)
       }
-      event.target.classList.add('activeDay');
+      event.target.classList.add(classForChange);
     }
     for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-      if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
+      if (elementButtonDayOfWeek[i].classList.contains(classForChange)) {
         index = i;
       }
     }
@@ -76,9 +78,9 @@ function switchButton(event) {
 
 function showDay(index) {
   for (var i = 0; i < elements.length; i++) {
-    if (elements[i].classList.contains('activex')) {
-      elements[i].classList.remove('activex')
-      elements[index].classList.add('activex')
+    if (elements[i].classList.contains(classForDay)) {
+      elements[i].classList.remove(classForDay)
+      elements[index].classList.add(classForDay)
       break
     }
   }
@@ -86,10 +88,10 @@ function showDay(index) {
 
 function showButton(position) {
   for (var i = 0; i < elementButtonDayOfWeek.length; i++) {
-    if (elementButtonDayOfWeek[i].classList.contains('activeDay')) {
-      elementButtonDayOfWeek[i].classList.remove('activeDay')
+    if (elementButtonDayOfWeek[i].classList.contains(classForChange)) {
+      elementButtonDayOfWeek[i].classList.remove(classForChange)
       break
     }
   }
-  elementButtonDayOfWeek[position].classList.add('activeDay');
+  elementButtonDayOfWeek[position].classList.add(classForChange);
 }
