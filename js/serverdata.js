da5e7916d7da1f7e27e3aae05d2dfbfab3924eff
DmitriyKorepanov <dmitriykorepanovs@gmail.com>
2018-09-11 00:15:02 +0400

    update

--- js/serverdata.js
+++ js/serverdata.js
@@ -32,16 +32,41 @@ function render(renderData) {
 }
 
 function transformFiveDays(str) {
-  var parsedArray = JSON.parse(str)
-
-  var renderData = {};
+  var parsedArray = JSON.parse(str);
+  console.log(parsedArray)
+  var renderData = {
+    temp: parsedArray.list
+  };
 
   renderFiveDays(renderData);
 }
 
 function renderFiveDays(renderData) {
+
+  var arrayProp = document.getElementById('two-items-temperature');
+  var prop= arrayProp.querySelectorAll('.temp');
+  var height = arrayProp.querySelectorAll('.heightTemp');
+ 
+  var arrayPropPart2 = document.getElementById('two-items-temperaturePart2');
+  var propPart2= arrayPropPart2.querySelectorAll('.temp');
+  var heightPart2 = arrayPropPart2.querySelectorAll('.heightTemp');
+
+  for (var i = 0; i < 4; i++) {
+    prop[i].textContent =Math.round( renderData.temp[i].main.temp);
+    height[i].style.height=Math.round( renderData.temp[i].main.temp)+"px";
+   }
+   for (var i = 0; i < 4; i++) {
+    propPart2[i].textContent =Math.round( renderData.temp[i+4].main.temp);
+    heightPart2[i].style.height=Math.round( renderData.temp[i+4].main.temp)+"px";
+   }
+
   var temperature8days = document.getElementById('dayWeather8days');
-  var dayWeather = temperature8days.querySelectorAll('.day-weather-temperature__nextday');
+  var dayWeather = temperature8days.querySelectorAll('.tempDay');
+
+for (var i = 0; i < 8; i++) {
+  dayWeather[i].textContent =Math.round( renderData.temp[i*4].main.temp)+'C';
+ }
+
 }
 
 function transformPollution(str) {
@@ -88,6 +113,9 @@ var city = `http://api.openweathermap.org/data/2.5/weather?q=Izhevsk&units=metri
 loadArray(transform, city)
 var pollution = `http://api.openweathermap.org/pollution/v1/co/56,53/current.json?appid=e2c078e26648e8e09b6e90e982007c80`;
 loadArray(transformPollution, pollution)
+var cityDefault = `http://api.openweathermap.org/data/2.5/forecast?q=Izhevsk&units=metric&APPID=e2c078e26648e8e09b6e90e982007c80`
+loadArray(transformFiveDays, cityDefault)
+
 
 var findCityInput = document.getElementById('findCityInput');
 findCityInput.addEventListener("change", changeCity);
